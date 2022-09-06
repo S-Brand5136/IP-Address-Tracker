8136dc9d9199141aefeecd092b556fe5c7ed4cd6
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-09-05 20:42:52 -0600

    Create helper to get Location data from API

---
+++ src/helpers/getLocationData.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const getLocationData = async (ipAddress) => {
+  try {
+    const { data } = await axios.get(
+      `https://geo.ipify.org/api/v2/country?ipAdress=${ipAddress}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+
+    return data;
+  } catch (error) {
+    console.error("here");
+    return error;
+  }
+};
